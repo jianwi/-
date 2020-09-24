ba1a55713d33104dd8eb7c8509fb331c41333afb
dujianjunn <v@jianwi.cn>
2020-09-24 23:59:57 +0800

    nodejs服务器cors跨域资源共享

--- nodejs/helloworld.js
+++ nodejs/helloworld.js
@@ -2,12 +2,13 @@ const http = require('http')
 const hostname = '127.0.0.1'
 const port = '3000'
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     res.statusCode = 200
-    res.setHeader('Content-Type','text/plain')
+    res.setHeader('Content-Type', 'text/plain')
+    res.setHeader('Access-Control-Allow-Origin','*')
     res.end("你好，世界\n")
 })
 
-server.listen(port,hostname,()=>{
-    console.log("服务器已经运行")
+server.listen(port, hostname, () => {
+    console.log("服务器已经运行于" + hostname + ':' + port)
 })
